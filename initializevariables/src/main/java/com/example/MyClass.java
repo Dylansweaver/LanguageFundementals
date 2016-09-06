dcde5de094623e79dbed36fb5365d35e0e1cbabf
Dylansweaver <dweaver@e112-m-18.mchenry.edu>
2016-09-06 13:10:01 -0500

    LanguageFundementals

---
+++ initializevariables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class MyClass
+{
+
+
+    public static void main(String[] args)
+    {
+        byte owThatHurt = 100 ;
+        short notTall = 32000;
+        int wibblyWobbly = 42;
+        long longAgo = 800000;
+        float notTheRootBeerKind = 24;
+        double trouble = 42.2;
+        boolean shmoolean = false ;
+        char charmander= 'x';
+
+        System.out.println("double= "+trouble);
+        System.out.println("byte= "+owThatHurt);
+        System.out.println("short= "+notTall);
+        System.out.println("int= "+wibblyWobbly);
+        System.out.println("long= "+longAgo);
+        System.out.println("float= "+notTheRootBeerKind);
+        System.out.println("boolean= "+shmoolean);
+        System.out.println("char= "+charmander);
+    }
+}
